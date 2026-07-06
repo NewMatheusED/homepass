fc01c46a742dbd339f6e72ab6d02f656d8a10c5c
Matheus Eduardo <matheus081105@gmail.com>
2026-07-05 21:43:14 -0300

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
-        if ($this->app->environment("production")) {
-            URL::forceScheme("https");
-        }
     }
 }
